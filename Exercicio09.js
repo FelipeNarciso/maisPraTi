bdec6bdbebd5059b7228271a283130637a653f09
FelipeNarciso <luisfelipenb88@gmail.com>
2024-05-05 23:17:46 -0300

    commit

--- Exercicio09.js
+++ Exercicio09.js
@@ -1,39 +1,37 @@
-//Aplicativo ler salários de homens e mulheres//
+//Aplicativo para ler salários de homens e mulheres//
 
 const prompt = require('prompt-sync')()
 
 let salarioH = 0
 let salarioM = 0
-console.log('Qual o sexo do funcionário?')
-console.log('Digite 0 para cancelar')
-console.log('Digite 1 para homem')
-console.log('Digite 2 para mulher')
-let sexo = prompt('Escolha uma alternativa: ')
 
-if(sexo > 2 || sexo < 0){
-    console.log('[ERRO] Alternativa inválida!')
-    console.log('Qual o sexo do funcionário?')
-    console.log('Digite 0 para cancelar')
-    console.log('Digite 1 para homem')
-    console.log('Digite 2 para mulher')
-    let sexo = prompt('Escolha uma alternativa: ')
-}else{
-    while(sexo != 0){
-        let salario = prompt('Qual o salário desse funcionário? ')
-        if(sexo == 1){
-            salarioH +=  salario
+let sexo = parseInt(prompt('Qual o sexo do funcionário? (1 para HOMEM e 2 para MULHER) '))
+let salario = parseFloat(prompt('Digite o salário do funcinário: '))
+
+while(sexo != 0){
+    if(sexo == 1){
+        salarioH += salario
+        console.log('-----------------------')
+        sexo = parseInt(prompt('Qual o sexo do funcionário? (1 para HOMEM e 2 para MULHER e 0 para ENCERRAR) '))
+        if(sexo == 0){
+            console.log(`salario homens igual ${salarioH} e mulhre igual ${salarioM}`)
         }else{
-            salarioM += salario
+            salario = parseFloat(prompt('Digite o salário do funcinário: '))
         }
-        console.log('---------------------------')
-        console.log('Qual o sexo do funcionário?')
-        console.log('Digite 0 para cancelar')
-        console.log('Digite 1 para homem')
-        console.log('Digite 2 para mulher')
-        sexo = prompt('Escolha uma alternativa: ')
+    }else if(sexo == 2){
+        salarioM += salario
+        console.log('-----------------------')
+        sexo = parseInt(prompt('Qual o sexo do funcionário? (1 para HOMEM e 2 para MULHER e 0 para ENCERRAR) '))
+        if(sexo == 0){
+            console.log(`salario homens igual ${salarioH} e mulhre igual ${salarioM}`)
+        }else{
+            salario = parseFloat(prompt('Digite o salário do funcinário: '))
+        }
+    }else{
+        console.log('[ERRO] Escolha uma alternativa disponível!')
+        console.log('-----------------------')
+        sexo = parseInt(prompt('Qual o sexo do funcionário? (1 para HOMEM e 2 para MULHER e 0 para ENCERRAR) '))
+        salario = parseFloat(prompt('Digite o salário do funcinário: '))
     }
-
 }
-console.log(`O total de salário pago aos homens é ${salarioH} e o total de salário pago as mulheres é ${salarioM}`)
-
-
+console.log(`O total de salário pago aos homens é R$ ${salarioH.toFixed(2)} e o total de salário pago as mulheres é R$${salarioM.toFixed(2)}`)
